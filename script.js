712f11af7f8a20532860b42146f834c415e9efe0
arielmusa <arielmusainfo@gmail.com>
2025-04-02 15:40:26 +0200

    remove old comment

--- script.js
+++ script.js
@@ -3,14 +3,6 @@
 - per i multipli di 5 stampi "Buzz" al posto del numero
 - per i multipli di 3 e di 5 stampi "FizzBuzz" al posto del numero */
 
-/* for (i = 1; i <= 100; i++) {
-  if (i % 3 !== 0) console.log(i);
-  if (i % 5 !== 0) console.log(i);
-  if (i % 3 === 0) console.log("Fizz");
-  if (i % 5 === 0) console.log("Buzz");
-  if (i % 3 && i % 5 === 0) console.log("FizzBuzz");
-} */
-
 const multThree = "Fizz";
 const multFive = "Buzz";
 const multThreeFive = "FizzBuzz";
